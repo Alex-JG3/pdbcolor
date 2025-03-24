02750246473363b2bfd1a2e55b19178c8b4faa2f
Alex Gregory <alex.gregory1@outlook.com>
2025-03-24 17:24:55 +0000

    Customise do_list to ensure lines are highlighted properly

--- pdbcolor/__init__.py
+++ pdbcolor/__init__.py
@@ -1,7 +1,7 @@
 from pdb import Pdb
 import sys
+import linecache
 
-import pygments
 from pygments import highlight
 from pygments.lexers import PythonLexer
 from pygments.lexer import RegexLexer
@@ -31,9 +31,10 @@ class PdbColor(Pdb):
         lines = lines_highlighted.split("\n")
         return lines
 
-    def _print_lines(self, lines, start, breaks=(), frame=None):
+    def _print_lines(self, lines, start, breaks=(), frame=None, highlight=True):
         """Print a range of lines."""
-        lines = self.highlight_lines(lines)
+        if highlight:
+            lines = self.highlight_lines(lines)
         if frame:
             current_lineno = frame.f_lineno
             exc_lineno = self.tb_lineno.get(frame, -1)
@@ -55,6 +56,68 @@ class PdbColor(Pdb):
             formatted_lines.append(s + "\t" + line.rstrip())
         for line in formatted_lines:
             self.message(line)
+
+    def do_list(self, arg):
+        """l(ist) [first [,last] | .]
+
+        List source code for the current file.  Without arguments,
+        list 11 lines around the current line or continue the previous
+        listing.  With . as argument, list 11 lines around the current
+        line.  With one argument, list 11 lines starting at that line.
+        With two arguments, list the given range; if the second
+        argument is less than the first, it is a count.
+
+        The current line in the current frame is indicated by "->".
+        If an exception is being debugged, the line where the
+        exception was originally raised or propagated is indicated by
+        ">>", if it differs from the current line.
+        """
+        self.lastcmd = "list"
+        last = None
+        if arg and arg != ".":
+            try:
+                if "," in arg:
+                    first, last = arg.split(",")
+                    first = int(first.strip())
+                    last = int(last.strip())
+                    if last < first:
+                        # assume it's a count
+                        last = first + last
+                else:
+                    first = int(arg.strip())
+                    first = max(1, first - 5)
+            except ValueError:
+                self.error("Error in argument: %r" % arg)
+                return
+        elif self.lineno is None or arg == ".":
+            first = max(1, self.curframe.f_lineno - 5)
+        else:
+            first = self.lineno + 1
+        if last is None:
+            last = first + 10
+        filename = self.curframe.f_code.co_filename
+        breaklist = self.get_file_breaks(filename)
+        try:
+            lines = linecache.getlines(filename, self.curframe.f_globals)
+
+            # Highlight lines before '_print_lines' to ensure they are
+            # highlighted correctly
+            lines = self.highlight_lines(lines)
+
+            self._print_lines(
+                lines[first - 1 : last],
+                first,
+                breaklist,
+                self.curframe,
+                highlight=False,
+            )
+            self.lineno = min(last, len(lines))
+            if len(lines) < last:
+                self.message("[EOF]")
+        except KeyboardInterrupt:
+            pass
+
+    do_l = do_list
 
 
 class CurrentLineFilter(Filter):
@@ -100,7 +163,6 @@ class LineNumberFilter(Filter):
                 previous_token_was_newline = False
             else:
                 yield ttype, value
-
 
 
 class PdbLexer(RegexLexer):
